0524ab34b87db49b82adfd89ee6600ffdad8df01
DoubleH-pku <2400016629hh@gmail.com>
2025-07-28 23:16:29 +0800

    Finish lab13.

--- Lab/lab13/data.sql
+++ Lab/lab13/data.sql
@@ -1,4 +1,3 @@
-
 -- DATA --
 
 CREATE TABLE flights AS
--- Lab/lab13/lab13.sql
+++ Lab/lab13/lab13.sql
@@ -2,17 +2,24 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, avg(MSRP) as average_price from products 
+  GROUP by category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, min(price) from inventory
+  group by item;
 
+
+create table best_value_for_money_products as
+  select name as name, min(MSRP/rating) from products
+  group by category;
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name, b.store as store from best_value_for_money_products as a, lowest_prices as b
+  where a.name = b.item GROUP by item;
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT sum(b.Mbs) from shopping_list as a, stores as b
+  where a.store = b.store;
